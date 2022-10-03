a5fbe0282eec6e8ebc1424bfcae661e94ef690a9
akhilchowdary8 <77147834+akhilchowdary8@users.noreply.github.com>
2022-10-03 15:06:11 +0530

    Add files via upload

---
+++ signup.php
@@ -0,0 +1,27 @@
+<?php
+$servername="localhost";
+$database="college";
+$username="root";
+$password="";
+$conn=mysqli_connect($servername,$username,$password,$database);
+if(isset($_POST['sub']))
+{
+$email=$_POST['email'];
+$password=$_POST['psw'];
+$roll_no=$_POST['roll_no'];
+$sql_query="INSERT INTO `profile`(`college_id`, `first`, `last`, `e-mail`, `phone`, `country`, `language`, `password`) VALUES ('$roll_no','','','$email','','','','$password')";
+if (mysqli_query($conn,$sql_query))
+{
+echo "registered successfully ";
+echo "please login in login page";
+echo"<a href='LoRe.html'> GO TO LOGINPAGE</a>";
+ 
+}
+else
+{
+echo "login failed";
+echo"<a href='LoRe.html'>back to registration page</a>";
+}
+mysqli_close($conn);
+}
+?>
